11ab5b28e11207c9b29ff6c7d53272ed77f263bc
Jonas Brandstötter <jonas.brandstoetter@akquinet.at>
2025-08-11 13:21:27 +0200

    update prompt colours

--- shell/.config/fish/conf.d/06-prompt.fish
+++ shell/.config/fish/conf.d/06-prompt.fish
@@ -1,15 +1,15 @@
 function fish_prompt
   set_color brblack
   echo -n "["(date "+%H:%M")"] "
-  set_color blue
+  set_color brblue
   echo -n (command -q hostname; and hostname; or hostnamectl hostname)
   if [ $PWD != $HOME ]
     set_color brblack
     echo -n ":"
-    set_color yellow
+    set_color brmagenta
     echo -n (basename $PWD)
   end
-  set_color green
+  set_color brcyan
   printf '%s ' (__fish_git_prompt)
   set_color red
   echo -n '| '
