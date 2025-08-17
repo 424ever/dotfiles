45f5be257db612f311c677064ac787a8756c75a1
Jonas Brandstötter <jonas.brandstoetter@gmx.at>
2025-08-17 10:51:10 +0200

    update prompt again

--- shell/.config/fish/conf.d/02-path.fish
+++ shell/.config/fish/conf.d/02-path.fish
@@ -1,6 +1,6 @@
 if status is-interactive
-  fish_add_path $HOME/bin
-  fish_add_path $HOME/.local/{bin,games}
-  fish_add_path $HOME/.cargo/bin
-  fish_add_path $HOME/go/bin
+  fish_add_path ~/bin
+  fish_add_path ~/.local/{bin,games}
+  fish_add_path ~/.cargo/bin
+  fish_add_path ~/go/bin
 end
